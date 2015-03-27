a715355202047823c080d62b840fb441e8871ebe
NOSC <dev@nosc.name>
2015-03-27 17:01:09 +0300

    Classes added

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,19 +5,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javax.xml.bind.JAXBException;
+
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        String a = System.getProperty("os.name");
+        System.out.println(a);
+
+        Parent root = FXMLLoader.load(getClass().getResource("forms/start.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.setResizable(false);
         primaryStage.show();
+
     }
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JAXBException {
         launch(args);
     }
+
+
 }
